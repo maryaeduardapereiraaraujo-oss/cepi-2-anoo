b784b79ef808badea521b3e38e78237233be8767
maryaeduardapereiraaraujo-oss <maryaeduardapereiraaraujo@gmail.com>
2026-04-27 11:25:10 -0300

    Add files via upload

---
+++ projeto-programacao/Triangulo.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace maryaatividades1
+{
+    internal class Triangulo
+    {
+        public double A;
+        public double B;
+        public double C;
+
+        public double Area()
+        {
+            double p = (A + B + C);
+            //double raiz = Math,Sqrt
+            return Math.Sqrt(p * (p - A) * (p - B) * (p - C));
+        }
+    }
+}
---
+++ projeto-programacao/program2.cs
@@ -0,0 +1,44 @@
+﻿using maryaatividades1;
+using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace program2
+{
+    internal class Program2
+    {
+        static void Main(string[] args)
+        {
+            Triangulo x, y;
+
+            x = new Triangulo();
+            y = new Triangulo();
+
+            Console.WriteLine("Entre com as medidas do X: ");
+            x.A = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            x.B = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            x.C = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+            Console.WriteLine("Entre com as medidas do y: ");
+            y.A = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            y.B = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            y.C = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+            double areaX = x.Area();//Funçao matematica raiz > math.Sqrt
+
+            double areaY = y.Area();//Funçao matematica raiz > math.Sqrt
+
+            Console.WriteLine("Area de X = " + areaX.ToString("F4", CultureInfo.InvariantCulture));
+            Console.WriteLine("Area de Y = " + areaY.ToString("F4", CultureInfo.InvariantCulture));
+
+
+
+
+
+        }
+    }
+}
